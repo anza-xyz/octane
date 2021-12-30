83099219776ed416b633042f3e2c75406385cd6e
Jordan Sexton <jordan@jordansexton.com>
2021-12-30 14:14:26 -0600

    reject on any error

--- src/middleware/wrapExpressHandler.ts
+++ src/middleware/wrapExpressHandler.ts
@@ -1,11 +1,12 @@
 import type { VercelApiHandler, VercelRequest, VercelResponse } from '@vercel/node';
 import { RequestHandler } from 'express';
 
+// Wrap an Express middleware function for compatibility with Vercel
 export const wrapExpressHandler = function (handler: RequestHandler): VercelApiHandler {
     return function (request: VercelRequest, response: VercelResponse): Promise<void> {
         return new Promise<void>(function (resolve, reject) {
             handler(request as any, response as any, function (error?: any) {
-                if (error instanceof Error) {
+                if (error) {
                     reject(error);
                 } else {
                     resolve();
