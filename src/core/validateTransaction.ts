f4c0c0573967da274c10761b3d69fe9e910c13b2
Jordan Sexton <jordan@jordansexton.com>
2022-01-01 14:59:38 -0600

    read config from json

--- src/core/validateTransaction.ts
+++ src/core/validateTransaction.ts
@@ -1,6 +1,7 @@
 import { Transaction } from '@solana/web3.js';
+import config from '../../config.json';
 import { connection } from './connection';
-import { ENV_FEE_PAYER, ENV_LAMPORTS_PER_SIGNATURE } from './env';
+import { ENV_FEE_PAYER } from './env';
 
 // Check that a transaction is basically valid
 export async function validateTransaction(transaction: Transaction): Promise<void> {
@@ -11,13 +12,20 @@ export async function validateTransaction(transaction: Transaction): Promise<voi
     // Check Octane's RPC node for the blockhash to make sure it's synced and the fee is reasonable
     const feeCalculator = await connection.getFeeCalculatorForBlockhash(transaction.recentBlockhash);
     if (!feeCalculator.value) throw new Error('blockhash not found');
-    if (feeCalculator.value.lamportsPerSignature > ENV_LAMPORTS_PER_SIGNATURE) throw new Error('fee too high');
+    if (feeCalculator.value.lamportsPerSignature > config.lamportsPerSignature) throw new Error('fee too high');
 
-    // Check the signatures to make sure the first is the fee payer (empty) and the second is the user (nonempty)
-    if (transaction.signatures.length > 2) throw new Error('too many signatures');
-    if (transaction.signatures[0].signature) throw new Error('invalid fee payer signature');
-    if (!transaction.signatures[0].publicKey.equals(ENV_FEE_PAYER)) throw new Error('invalid fee payer pubkey');
-    if (!transaction.signatures[1].signature) throw new Error('missing signature');
+    // Check the signatures for length, the primary signature, and secondary signature(s)
+    if (!transaction.signatures.length) throw new Error('no signatures');
+    if (transaction.signatures.length > config.maxSignatures) throw new Error('too many signatures');
+
+    const [primary, ...secondary] = transaction.signatures;
+    if (!primary.publicKey.equals(ENV_FEE_PAYER)) throw new Error('invalid fee payer pubkey');
+    if (primary.signature) throw new Error('invalid fee payer signature');
+
+    for (const signature of secondary) {
+        if (!signature.publicKey) throw new Error('missing public key');
+        if (!signature.signature) throw new Error('missing signature');
+    }
 
     // Prevent draining by making sure that the fee payer isn't provided as writable or a signer to any instruction
     for (const instruction of transaction.instructions) {
