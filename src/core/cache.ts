ef0c24bc28bc4485b5ab2e8c6abdba79e0d1ba6c
steveluscher <github@steveluscher.com>
2022-01-12 22:14:09 -0800

    Import the memory store explicitly

--- src/core/cache.ts
+++ src/core/cache.ts
@@ -1,3 +1,6 @@
+// @ts-ignore (TS7016) There is no type definition for this at DefinitelyTyped.
+import MemoryStore from 'cache-manager/lib/stores/memory';
+
 import cacheManager from 'cache-manager';
 
-export const cache = cacheManager.caching({ store: 'memory', max: 1000, ttl: 120 /*seconds*/ });
+export const cache = cacheManager.caching({ store: MemoryStore, max: 1000, ttl: 120 /*seconds*/ });
